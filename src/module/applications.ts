285f63f3d9b17cb4e362415d383db4161f808978
Sophia Pearson <codergal89@gmail.com>
2022-02-28 09:38:31 +0100

    sta-ng: extract initialization functions

---
+++ src/module/applications.ts
@@ -0,0 +1,71 @@
+import { STACharacterSheet } from "./actors/sheets/character-sheet.js";
+import { STAExtendedTaskSheet } from "./actors/sheets/extended-task-sheet.js";
+import { STASmallCraftSheet } from "./actors/sheets/smallcraft-sheet.js";
+import { STAStarshipSheet } from "./actors/sheets/starship-sheet.js";
+import { STAArmorSheet } from "./items/armor-sheet.js";
+import { STACharacterWeaponSheet } from "./items/character-weapon-sheet.js";
+import { STAGenericSheet } from "./items/generic-sheet.js";
+import { STAItemSheet } from "./items/item-sheet.js";
+import { STASmallCraftContainerSheet } from "./items/smallcraftcontainer-sheet.js";
+import { STAStarshipWeaponSheet } from "./items/starship-weapon-sheet.js";
+import { STATalentSheet } from "./items/talent-sheet.js";
+
+export function registerApplications() : void {
+  registerActorApplications();
+  registerItemApplications();
+}
+
+/**
+ * Register out system's actor applications (sheets) with the Foundry infrastructure
+ */
+ function registerActorApplications(): void {
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("sta-ng", STACharacterSheet, {
+    types: ["character"],
+    makeDefault: true
+  });
+  Actors.registerSheet("sta-ng", STAStarshipSheet, {
+    types: ["starship"]
+  });
+  Actors.registerSheet("sta-ng", STASmallCraftSheet, {
+    types: ["smallcraft"],
+  });
+  Actors.registerSheet("sta-ng", STAExtendedTaskSheet, {
+    types: ["extendedtask"]
+  });
+}
+
+/**
+ * Register out system's item applications (sheets) with the Foundry infrastructure
+ */
+ function registerItemApplications(): void {
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("sta-ng", STAItemSheet, {
+    types: ["item"],
+    makeDefault: true
+  });
+  Items.registerSheet("sta-ng", STACharacterWeaponSheet, {
+    types: ["characterweapon"],
+  });
+  Items.registerSheet("sta-ng", STAStarshipWeaponSheet, {
+    types: ["starshipweapon"],
+  });
+  Items.registerSheet("sta-ng", STAArmorSheet, {
+    types: ["armor"],
+  });
+  Items.registerSheet("sta-ng", STATalentSheet, {
+    types: ["talent"],
+  });
+  Items.registerSheet("sta-ng", STAGenericSheet, {
+    types: ["value"],
+  });
+  Items.registerSheet("sta-ng", STAGenericSheet, {
+    types: ["focus"],
+  });
+  Items.registerSheet("sta-ng", STAGenericSheet, {
+    types: ["injury"],
+  });
+  Items.registerSheet("sta-ng", STASmallCraftContainerSheet, {
+    types: ["smallcraftcontainer"],
+  });
+}
---
+++ src/module/settings.ts
@@ -0,0 +1,66 @@
+/**
+ * Register out system's settings with the Foundry infrastructure
+ */
+export function registerSystemSettings(): void {
+  game.settings.register("sta-ng", "multipleComplications", {
+    name: "Multiple Complications:",
+    hint: "The rulebook states \"Any die which rolled 20 causes a complication\". This is slightly unclear and as of Version 8 of the PDF, this is still not clear - likely due to the incredible rarity. Enabling this will allow roles to display \"There were x Complications\" if multiple 20s are rolled. Disabling will just state a single complication.",
+    scope: "world",
+    type: Boolean,
+    default: true,
+    config: true
+  });
+
+  game.settings.register("sta-ng", "threatPermissionLevel", {
+    name: "Threat Tracker User Role:",
+    hint: "Who should be allowed to amend the threat tracker?",
+    scope: "world",
+    type: Number,
+    default: 3,
+    config: true,
+    choices: {
+      [CONST.USER_ROLES.PLAYER]: "Players",
+      [CONST.USER_ROLES.TRUSTED]: "Trusted Players",
+      [CONST.USER_ROLES.ASSISTANT]: "Assistant Gamemaster",
+      [CONST.USER_ROLES.GAMEMASTER]: "Gamemasters",
+    }
+  });
+
+  game.settings.register("sta-ng", "momentumPermissionLevel", {
+    name: "Momentum Tracker User Role:",
+    hint: "Who should be allowed to amend the momentum tracker?",
+    scope: "world",
+    type: Number,
+    default: 1,
+    config: true,
+    choices: {
+      [CONST.USER_ROLES.PLAYER]: "Players",
+      [CONST.USER_ROLES.TRUSTED]: "Trusted Players",
+      [CONST.USER_ROLES.ASSISTANT]: "Assistant Gamemaster",
+      [CONST.USER_ROLES.GAMEMASTER]: "Gamemasters",
+    }
+  });
+
+  game.settings.register("sta-ng", "maxNumberOfReputation", {
+    name: "Maximum amount of Reputation:",
+    hint: "Max number of reputation that can be given to a character. 10 is default.",
+    scope: "world",
+    type: Number,
+    default: 10,
+    config: true
+  });
+
+  game.settings.register("sta-ng", "threat", {
+    scope: "world",
+    type: Number,
+    default: 0,
+    config: false
+  });
+
+  game.settings.register("sta-ng", "momentum", {
+    scope: "world",
+    type: Number,
+    default: 0,
+    config: false
+  });
+}
