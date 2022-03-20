7de58c1f8755076f39787dc12529b3f0f1edb8d6
Sophia Pearson <codergal89@gmail.com>
2022-03-20 22:35:18 +0100

    items: add type definition for species

---
+++ src/module/items/SpeciesData.d.ts
@@ -0,0 +1,17 @@
+interface SpeciesDataSourceData extends ItemCommonDataSourceData {
+  eras: Era[]
+  exampleValue: string
+  traitDescription: string
+}
+
+interface SpeciesDataSource {
+  type: "species"
+  data: SpeciesDataSourceData
+}
+
+type SpeciesDataPropertiesData = SpeciesDataSourceData;
+
+interface SpeciesDataProperties {
+  type: "species"
+  data: SpeciesDataPropertiesData
+}
